a0b6e1f2e016c714ec2c11858c851a58b6b21458
Mikias Delelegn <miku123delelegn@gmail.com>
2024-09-21 14:24:25 -0700

    creat python file

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 5
+number2 = 10
+print("Addition of 10 and 5 is",number1 + number2)
+print("Subtraction of 10 and 5 is" ,number2 - number1)
+print("Multiplication of 10 and 5 is" ,number1*number2)
+      
+
+
+
+
+
