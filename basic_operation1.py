3b7b72aaac554949f97da26f6405d65c80394036
miku123711 <miku123delelegn@gmail.com>
2024-09-26 13:04:11 -0700

    Add files via upload

---
+++ basic_operation1.py
@@ -0,0 +1,15 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py", line 2, in <module>
+    print(number)
+NameError: name 'number' is not defined. Did you mean: 'number1'?
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+10
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+10
+5
