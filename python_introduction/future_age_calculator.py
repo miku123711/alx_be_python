6e59fc69d518dc90f04c8dccc6b108a11dd2b5c5
miku123711 <miku123delelegn@gmail.com>
2024-09-26 14:13:09 -0700

    Add files via upload

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,26 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+27
+>>> 
+= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+32
+>>> 
+= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+32
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py", line 3, in <module>
+    print("In 2050, you will be " + str(age_in_2050) + " years old.")
+NameError: name 'age_in_2050' is not defined. Did you mean: 'Age_in_2023'?
+>>> 
+= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+32
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py", line 3, in <module>
+    print("In 2050, you will be " + 32+27 + " years old.")
+TypeError: can only concatenate str (not "int") to str
+>>> 
+= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+32
+In 2050, you will be 59 years old.
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,29 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py
+1000
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+1000
+0.05
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+1000
+0.05
+3
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+1000
+0.05
+3
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py", line 7, in <module>
+    I=prinicipal*rate*time
+NameError: name 'prinicipal' is not defined. Did you mean: 'principal'?
+>>> 
+== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/python 113.py ==
+1000
+0.05
+3
+The simple interest is: 150.0
