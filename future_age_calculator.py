e2d6614e4c3a07123ef5dc696430461c3a6fd5ad
miku123711 <miku123delelegn@gmail.com>
2024-09-26 16:11:04 -0700

    Add files via upload

---
+++ future_age_calculator.py
@@ -0,0 +1,30 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+How old are you?
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+How old are you?
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+12
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+12
+14
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+How old are you?
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+How old are you?
+Your age in 2023 is: 32
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+How old are you?
+Your age in 2023 is: 32
+In 2050, you will be59 years old.
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/qwa.py =====
+How old are you?
+Your age in 2023 is: 32
+In 2050, you will be59 years old.
