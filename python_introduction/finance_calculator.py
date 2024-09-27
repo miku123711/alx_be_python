416fd1ecb3cc87eebc086562f274a8cfdb1c085d
miku123711 <miku123delelegn@gmail.com>
2024-09-26 22:51:34 -0700

    Add files via upload

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,131 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py", line 5, in <module>
+    monthly_savings = monthly_income - monthly_expenses
+TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py", line 5, in <module>
+    monthly_savings = monthly_income - monthly_expenses
+TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+1000
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+1000
+1
+
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+1
+Traceback (most recent call last):
+  File "C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py", line 17, in <module>
+    print("Your monthly savings are $"+5000-4000)
+TypeError: can only concatenate str (not "int") to str
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+1
+Your monthly savings are $1000
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+1
+Your monthly savings are $1000
+Projected savings after one year, with interest, is: Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+>>> 
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/fan.py =====
+Enter your monthly income:
+Enter your total monthly expenses:
+monthly_income - monthly_expenses
+0.05
+Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+5000
+4000
+1
+Your monthly savings are $1000
+Projected savings after one year, with interest, is: Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+Projected savings after one year, with interest, is: 12600.0
--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,26 +1,14 @@
 Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
-27
+===== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/added.py ====
+How old are you?
 >>> 
-= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+===== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/added.py ====
+How old are you?
 32
 >>> 
-= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
+===== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/added.py ====
+How old are you?
 32
-Traceback (most recent call last):
-  File "C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py", line 3, in <module>
-    print("In 2050, you will be " + str(age_in_2050) + " years old.")
-NameError: name 'age_in_2050' is not defined. Did you mean: 'Age_in_2023'?
->>> 
-= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
-32
-Traceback (most recent call last):
-  File "C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py", line 3, in <module>
-    print("In 2050, you will be " + 32+27 + " years old.")
-TypeError: can only concatenate str (not "int") to str
->>> 
-= RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/hours_to_seconds.py
-32
-In 2050, you will be 59 years old.
+In 2050, you will be59years old.
