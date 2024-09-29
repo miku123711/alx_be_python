1e23f57fc168f9c09ca8450f813ea055e4203a19
miku123711 <miku123delelegn@gmail.com>
2024-09-28 22:42:09 -0700

    Add files via upload

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,19 +1,20 @@
 Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-= RESTART: C:/Users/op/Desktop/simple_interest.py
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/1q.py ======
 10
 >>> 
-================ RESTART: C:/Users/op/Desktop/simple_interest.py ===============
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/1q.py ======
 10
 5
 >>> 
-================ RESTART: C:/Users/op/Desktop/simple_interest.py ===============
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/1q.py ======
 10
 5
-50
+Area
 >>> 
-================ RESTART: C:/Users/op/Desktop/simple_interest.py ===============
+====== RESTART: C:/Users/op/AppData/Local/Programs/Python/Python312/1q.py ======
 10
 5
+Area
 The area of the rectangle is: 50
